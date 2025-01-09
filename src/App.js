944654f4f51042d0c7e233065319e6a983855f4b
ehsan hajihosseini <niamileo@gmail.com>
2025-01-09 14:26:05 +0330

    add routing with react-router-dom and create Home and About components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Home Page</p>
       </header>
     </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>About Page</p>
+      </header>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
