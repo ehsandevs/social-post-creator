de9ac25d891a59e2303fbc2bb9ed5b6215e69d51
ehsan hajihosseini <niamileo@gmail.com>
2025-01-16 13:30:40 +0330

    refactor App component to set InstagramPost as the default route

--- src/App.js
+++ src/App.js
@@ -2,32 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import InstagramPost from './Pages/Instagram';
 
-function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Home Page</p>
-      </header>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>About Page</p>
-      </header>
-    </div>
-  );
-}
-
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/instagram" element={<InstagramPost />} />
+      <Route path="/" element={<InstagramPost />} />
     </Routes>
   );
 }
